eeb06c73722bf44f47ffcc42995e4021dba48444
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-04-28 10:43:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const makeChange = (c) => {
   // your name here
 	let obj = {
-		["q": 0.25],
-		["d": 0.10],
-		["n": 0.05],
-		["p", 0.01]
+		"q": 0.25,
+		"d": 0.10,
+		"n": 0.05,
+		"p": 0.01
 	}
-	let change = [];
-		for (let i = 0; i <= 4; i++) {
-			let quotients = Math.floor(c / obj[i][1]);
-			if (quotients >= 0) {
-				change.push(obj[i][0]);
-			}
-		}
+	let change = {
+		"q": 0,
+		"d": 0,
+		"n": 0,
+		"p": 0
+	};
+	let amount = c/100;
+	
+	change.q = Math.floor(amount/obj.q);
+	amount %= obj.q;
+
+	change.d = Math.floor(amount/obj.d);
+	amount %= obj.d;
+
+	change.n = Math.floor(amount/obj.n);
+	amount %= obj.n;
+
+	change.p = Math.floor(amount/obj.p);
+	
 	return change;
 };
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
