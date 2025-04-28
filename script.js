6e3ffeb8ea335b8a83ef186fff0a0ab017751589
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-04-28 10:24:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let obj = {
+		["q": 0.25],
+		["d": 0.10],
+		["n": 0.05],
+		["p", 0.01]
+	}
+	let change = [];
+		for (let i = 0; i <= 4; i++) {
+			let quotients = Math.floor(c / obj[i][1]);
+			if (quotients >= 0) {
+				change.push(obj[i][0]);
+			}
+		}
+	return change;
 };
 
 // Do not the change the code below
